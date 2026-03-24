44a13b0c0711a6a4540228d2cb5f4d116c46f4b8
Priyanshu <jhapriyanshu1606@gmail.com>
2026-03-24 23:01:59 +0530

    fix(gateway): bug fixed

--- api-gateway/src/main/java/com/omnicharge/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/omnicharge/apigateway/ApiGatewayApplication.java
@@ -2,6 +2,7 @@ package com.omnicharge.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient

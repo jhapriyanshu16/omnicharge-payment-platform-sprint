a514c9654ea5bca206dea18c39ba4e877b49c95b
Priyanshu <jhapriyanshu1606@gmail.com>
2026-03-26 12:36:38 +0530

    feat(operator): implement plan management module with CRUD APIs, DTO mapping, validation rules, exception handling and operator-plan relationship

---
+++ operator-service/src/main/java/com/omnicharge/operatorservice/controller/PlanController.java
@@ -0,0 +1,60 @@
+package com.omnicharge.operatorservice.controller;
+
+import com.omnicharge.operatorservice.dto.request.CreatePlanRequest;
+import com.omnicharge.operatorservice.dto.response.ApiResponse;
+import com.omnicharge.operatorservice.dto.response.PlanResponse;
+import com.omnicharge.operatorservice.service.PlanService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/plans")
+@RequiredArgsConstructor
+public class PlanController {
+
+    private final PlanService service;
+
+    @PostMapping
+    public ApiResponse<PlanResponse> create(@Valid @RequestBody CreatePlanRequest request){
+
+        return ApiResponse.<PlanResponse>builder()
+                .success(true)
+                .message("Plan created")
+                .data(service.create(request))
+                .build();
+    }
+
+    @GetMapping("/operator/{operatorId}")
+    public ApiResponse<List<PlanResponse>> getByOperator(@PathVariable Long operatorId){
+
+        return ApiResponse.<List<PlanResponse>>builder()
+                .success(true)
+                .message("Plans list")
+                .data(service.getPlansByOperator(operatorId))
+                .build();
+    }
+
+    @GetMapping("/{id}")
+    public ApiResponse<PlanResponse> getById(@PathVariable Long id){
+
+        return ApiResponse.<PlanResponse>builder()
+                .success(true)
+                .message("Plan found")
+                .data(service.getById(id))
+                .build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ApiResponse<?> delete(@PathVariable Long id){
+
+        service.delete(id);
+
+        return ApiResponse.builder()
+                .success(true)
+                .message("Plan deleted")
+                .build();
+    }
+}

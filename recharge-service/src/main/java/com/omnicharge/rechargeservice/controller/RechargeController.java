80ac1b745ef37166ea11766220ba7294b0138edb
Priyanshu <jhapriyanshu1606@gmail.com>
2026-03-27 11:40:16 +0530

    feat(recharge): add initial recharge service setup with entity, APIs, persistence and exception handling

---
+++ recharge-service/src/main/java/com/omnicharge/rechargeservice/controller/RechargeController.java
@@ -0,0 +1,41 @@
+package com.omnicharge.rechargeservice.controller;
+
+import com.omnicharge.rechargeservice.dto.request.RechargeRequest;
+import com.omnicharge.rechargeservice.dto.response.ApiResponse;
+import com.omnicharge.rechargeservice.dto.response.RechargeResponse;
+import com.omnicharge.rechargeservice.service.RechargeService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/recharges")
+@RequiredArgsConstructor
+@Slf4j
+public class RechargeController {
+
+    private final RechargeService service;
+
+    @PostMapping
+    public ApiResponse<RechargeResponse> create(
+            @RequestHeader("X-User-Email") String userEmail,
+            @Valid @RequestBody RechargeRequest request){
+        log.info("User Email from header: {}", userEmail);
+        return ApiResponse.<RechargeResponse>builder()
+                .success(true)
+                .message("Recharge initiated")
+                .data(service.createRecharge(userEmail, request))
+                .build();
+    }
+
+    @GetMapping("/{id}")
+    public ApiResponse<RechargeResponse> get(@PathVariable Long id){
+
+        return ApiResponse.<RechargeResponse>builder()
+                .success(true)
+                .message("Recharge details")
+                .data(service.getById(id))
+                .build();
+    }
+}

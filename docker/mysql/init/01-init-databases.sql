3f245d0d878f4692a779cc68e287e0f7ebd2e2bd
Priyanshu <jhapriyanshu1606@gmail.com>
2026-03-29 13:44:38 +0530

    feat: add dockerization support and clean up build artifacts

---
+++ docker/mysql/init/01-init-databases.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS omnicharge_user_db;
+CREATE DATABASE IF NOT EXISTS omnicharge_operator_db;
+CREATE DATABASE IF NOT EXISTS omnicharge_recharge_db;
+CREATE DATABASE IF NOT EXISTS omnicharge_payment_db;

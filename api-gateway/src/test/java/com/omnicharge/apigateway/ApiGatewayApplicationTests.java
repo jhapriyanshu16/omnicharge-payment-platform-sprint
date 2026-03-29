49230beb09f2bc2373c53ae0bbb1723bbddf1477
Priyanshu <jhapriyanshu1606@gmail.com>
2026-03-29 23:02:20 +0530

    fixed some test issues for sonar cloud

--- api-gateway/src/test/java/com/omnicharge/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/omnicharge/apigateway/ApiGatewayApplicationTests.java
@@ -1,13 +1,18 @@
 package com.omnicharge.apigateway;
 
+import com.omnicharge.apigateway.security.JwtUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class ApiGatewayApplicationTests {
 
+    @MockBean
+    private JwtUtil jwtUtil; // This prevents the real JwtUtil from trying to initialize
+
     @Test
     void contextLoads() {
+        // The test will now pass because the Context found a 'mock' bean
     }
-
 }
